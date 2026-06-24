3ed525fe8a02460322be1c26f4e78278d75934e8
muhammadYasinBaloch <yaseenbalich71@gmail.com>
2026-06-25 01:16:29 +0500

    QUIz

---
+++ main.js
@@ -0,0 +1,17 @@
+const questions = [
+    { question: "2 + 2 = ?", answer: "4" },
+    { question: "Capital of Pakistan?", answer: "Islamabad" }
+];
+
+let score = 0;
+
+const checkAnswer = (userAnswer, correctAnswer) => 
+    userAnswer === correctAnswer ? score++ : score;
+
+questions.forEach(q => {
+    let userAnswer = prompt(q.question);
+    checkAnswer(userAnswer, q.answer);
+});
+
+const result = score >= 1 ? "Pass" : "Fail";
+alert(`Your Score: ${score}\nResult: ${result}`);
